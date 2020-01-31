0cc7851942dcaf206d3bc96aa198b927e5fe1b34
BTOCC24 <kisses6938@gmail.com>
2020-02-01 03:17:56 +0900

    2020-01-29

--- level1/2445.cpp
+++ level1/2445.cpp
@@ -1,34 +1,31 @@
-//20/1/29 23:00 2444
+//20/1/29 23:00 2445
 
 #include <iostream>
 using namespace std;
 
 int main() {
-	int floor;
+	int floor, star, blank;
 	cin >> floor;
-	int temp = floor-1;
-	int star = 1;
 	for (int i = 0; i < floor; i++) {
-		for (int j = 0; j < temp; j++)
+		star = i + 1;
+		blank = 2*(floor - star);
+		for (int j = 0; j < star; j++)
+			cout << '*';
+		for (int j = 0; j < blank; j++)
 			cout << ' ';
 		for (int j = 0; j < star; j++)
 			cout << '*';
-
-		temp--;
-		star += 2;
 		cout << '\n';
 	}
-
-	temp = 1;
-	star = 2 * floor - 3;
 	for (int i = 1; i < floor; i++) {
-		for (int j = 0; j < temp; j++)
+		star = floor-i;
+		blank = 2 * (floor - star);
+		for (int j = 0; j < star; j++)
+			cout << '*';
+		for (int j = 0; j < blank; j++)
 			cout << ' ';
 		for (int j = 0; j < star; j++)
 			cout << '*';
-
-		temp++;
-		star -= 2;
 		cout << '\n';
 	}
 }
