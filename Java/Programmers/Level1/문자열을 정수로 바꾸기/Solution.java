3098553642e3b67fcbdbe3eb9b4002f4b45efd0b
BTOCC24 <kisses6938@gmail.com>
2021-01-25 00:29:34 +0900

    study algo 21/01/25

--- Java/Programmers/Level1/문자열을 정수로 바꾸기/Solution.java
+++ Java/Programmers/Level1/문자열을 정수로 바꾸기/Solution.java
@@ -1,4 +1,20 @@
 class Solution {
-	public String solution(int n) {
-	}
+    public int solution(String s){
+        boolean sign = false;
+        char first = s.charAt(0);
+        if(first == '+' || first == '-'){
+            s = s.substring(1);
+            if(first == '-'){
+                sign = true;
+            }
+        }
+        int answer = 0;
+        for(int i=0;i<s.length();i++){
+            answer *= 10;
+            answer += s.charAt(i) - '0';
+        }
+        if(sign)
+            answer *= -1;
+        return answer;
+    }
 }
