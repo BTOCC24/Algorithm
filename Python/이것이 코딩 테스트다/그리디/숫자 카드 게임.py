2eb583f36d03b07f0699dd3b584bea7ee116ef0d
BTOCC24 <kisses6938@gmail.com>
2021-01-25 03:21:51 +0900

    study algo 21/01/25

--- Python/이것이 코딩 테스트다/그리디/숫자 카드 게임.py
+++
@@ -1,5 +0,0 @@
-n, m = map(int, input().split())
-
-cards = [min(list(map(int, input().split()))) for i in range(n)]
-
-print(max(cards))
--- Python/이것이 코딩 테스트다/다이나믹 프로그래밍/바닥 공사2.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-x = [0] * (n + 1)
-
-x[1] = 1
-x[2] = 3
-for i in range(3, n + 1):
-    x[i] = x[i-2]*2 + x[i-1]
-
-print(x[n])
