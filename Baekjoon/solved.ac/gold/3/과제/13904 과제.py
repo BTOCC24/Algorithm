d0b878f4590fca26d0f498d38f5afbe40de1cea7
BTOCC24 <kisses6938@gmail.com>
2021-02-15 23:38:20 +0900

    study algo 21/02/15

--- Baekjoon/solved.ac/gold/3/과제/13904 과제.py
+++ Baekjoon/solved.ac/gold/3/과제/13904 과제.py
@@ -1,23 +1,29 @@
 N = int(input())
 data = [list(map(int, input().split())) for _ in range(N)]
-
 data.sort()
-store = []
 
-count = 0  # 들어가 있는 수
-min_value = 0  # 들어간 것 중에 제일 작은 일 수
-period = data[0][0] - min_value  # 주어진 기한
-print(data)
+score = 0  # 점수
+base = 0  # 이전 과제 끝난 시점
+deadline = 0  # 과제 기한
+buffer = []  # 데이터 쌓일 곳
 
-# while data:
-while count <= period:
-    print(count, period)
-    homework = data[0]
-    date = homework[0]
-    period = date - min_value
-    while date == data[count][0]:
-        store.append(data.pop(0))
-        count += 1
+while data:
+    while len(data) > 0 and len(buffer) <= deadline:
+        day = data[0][0]
+        deadline = day - base
+        while len(data) > 0 and data[0][0] == day:
+            buffer.append(data.pop(0))
 
+    base = buffer[-1][0]
+    buffer.sort(key=lambda x: x[1], reverse=True)
 
-print(store)
+    print(deadline)
+    if deadline > len(buffer):
+        for i in range(len(buffer)):
+            score += buffer[i][1]
+    else:
+        for i in range(deadline):
+            score += buffer[i][1]
+    buffer = []
+
+print(score)
