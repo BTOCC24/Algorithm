be06c4e3244bb2bf933aa04416a88aea038c10d6
BTOCC24 <kisses6938@gmail.com>
2021-02-16 21:57:19 +0900

    study algo 21/02/16

--- Baekjoon/bronze/2/거스름돈.java
+++ Baekjoon/bronze/2/거스름돈.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hi~");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        System.out.println(solve(1000-input));
+        br.close();
+    }
+
+    static int solve(int input) {
+        int result = 0;
+        int[] arr = {500, 100, 50, 10, 5, 1};
+        for (int value : arr) {
+            if(input == 0) return result;
+            result += input / value;
+            input %= value;
+        }
+        return result;
     }
 }
