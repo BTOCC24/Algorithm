11889cd7ad05a7525bd53e39a34bfa51347001e2
BTOCC24 <kisses6938@gmail.com>
2021-03-23 00:07:59 +0900

    study algo 21/03/23

--- Baekjoon/gold/4/단어 수학.java
+++ Baekjoon/gold/4/단어 수학.java
@@ -10,20 +10,22 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] number = new int[27];
+        Integer[] number = new Integer[27];
+        for(int i = 0 ; i < 27; i ++){
+            number[i] = 0;
+        }
         for(int i = 0 ; i < n ; i ++){
             String input = br.readLine();
             for(int j = 0 ; j <input.length(); j ++){
                 int index = input.charAt(j) - 'A';
-                number[index] += Math.pow(10, input.length() - j - 1);
+                number[index] += (int)Math.pow(10, input.length() - j - 1);
             }
         }
-        Arrays.sort(number, Collections.reverseOrder());
+        Arrays.sort(number, (a, b) -> b - a);
         int result = 0;
         for(int i = 0; i < 10; i++){
             result += number[i] * (10 - i - 1);
         }
-        System.out.println(Arrays.toString(number));
         System.out.println(result);
     }
 }
