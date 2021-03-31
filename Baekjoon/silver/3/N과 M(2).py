9ae41a5fdaac7092926c8625b5c1fce56d10a739
BTOCC24 <kisses6938@gmail.com>
2021-03-31 23:57:55 +0900

    study algo 21/03/31

--- Baekjoon/silver/3/N과 M(2).py
+++ Baekjoon/silver/3/N과 M(2).py
@@ -1,13 +1,3 @@
-# from itertools import combinations
-#
-# n, m = map(int, input().split())
-#
-# li = [str(i + 1) for i in range(n)]
-# data = '\n'.join(list(map(' '.join, list(combinations(li, m)))))
-#
-# pp = [1,2,3,4]
-# print(*pp)
-
 n, m = map(int, input().split())
 visited = [False] * (n + 1)
 output = []
